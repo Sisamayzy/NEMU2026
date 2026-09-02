5fc777351a7138a93ad170f128307ce3c6679b2d
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-02 02:41:56 +0000

    >  compile NEMU 2020244008 young Linux 50446c482b5a 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:41:56 up  1:07,  0 users,  load average: 0.11, 0.06, 0.02 48e904538b7aea85bd55b12ee65c6dd99a39a3b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -10,63 +10,147 @@
 void cpu_exec(uint32_t);
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
-char* rl_gets() {
+char *rl_gets()
+{
 	static char *line_read = NULL;
 
-	if (line_read) {
+	if (line_read)
+	{
 		free(line_read);
 		line_read = NULL;
 	}
 
 	line_read = readline("(nemu) ");
 
-	if (line_read && *line_read) {
+	if (line_read && *line_read)
+	{
 		add_history(line_read);
 	}
 
 	return line_read;
 }
 
-static int cmd_c(char *args) {
+static int cmd_c(char *args)
+{
 	cpu_exec(-1);
 	return 0;
 }
 
-static int cmd_q(char *args) {
+static int cmd_si(char *args)
+{
+	int n = 1;
+
+	if (args != NULL)
+	{
+		sscanf(args, "%d", &n);
+	}
+
+	cpu_exec(n);
+
+	return 0;
+}
+
+static int cmd_q(char *args)
+{
 	return -1;
+}
+
+static int cmd_info(char *args)
+{
+	if (args == NULL)
+	{
+		printf("Usage: info r\n");
+		return 0;
+	}
+
+	if (strcmp(args, "r") == 0)
+	{
+		printf("eax\t0x%08x\n", cpu.eax);
+		printf("ecx\t0x%08x\n", cpu.ecx);
+		printf("edx\t0x%08x\n", cpu.edx);
+		printf("ebx\t0x%08x\n", cpu.ebx);
+		printf("esp\t0x%08x\n", cpu.esp);
+		printf("ebp\t0x%08x\n", cpu.ebp);
+		printf("esi\t0x%08x\n", cpu.esi);
+		printf("edi\t0x%08x\n", cpu.edi);
+		printf("eip\t0x%08x\n", cpu.eip);
+	}
+
+	return 0;
+}
+
+static int cmd_x(char *args)
+{
+	int n;
+	uint32_t addr;
+
+	if (args == NULL)
+	{
+		printf("Usage: x N EXPR\n");
+		return 0;
+	}
+
+	if (sscanf(args, "%d %x", &n, &addr) != 2)
+	{
+		printf("Usage: x N EXPR\n");
+		return 0;
+	}
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		uint32_t data = swaddr_read(addr + i * 4, 4);
+
+		printf("0x%08x: 0x%08x\n",
+			   addr + i * 4,
+			   data);
+	}
+
+	return 0;
 }
 
 static int cmd_help(char *args);
 
-static struct {
+static struct
+{
 	char *name;
 	char *description;
-	int (*handler) (char *);
-} cmd_table [] = {
-	{ "help", "Display informations about all supported commands", cmd_help },
-	{ "c", "Continue the execution of the program", cmd_c },
-	{ "q", "Exit NEMU", cmd_q },
+	int (*handler)(char *);
+} cmd_table[] = {
+	{"help", "Display informations about all supported commands", cmd_help},
+	{"c", "Continue the execution of the program", cmd_c},
+	{"q", "Exit NEMU", cmd_q},
 
 	/* TODO: Add more commands */
+	{"si", "Step instruction", cmd_si},
+	{"info", "Print register information", cmd_info},
+	{"x", "Examine memory", cmd_x},
 
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
-static int cmd_help(char *args) {
+static int cmd_help(char *args)
+{
 	/* extract the first argument */
 	char *arg = strtok(NULL, " ");
 	int i;
 
-	if(arg == NULL) {
+	if (arg == NULL)
+	{
 		/* no argument given */
-		for(i = 0; i < NR_CMD; i ++) {
+		for (i = 0; i < NR_CMD; i++)
+		{
 			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
 		}
 	}
-	else {
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(arg, cmd_table[i].name) == 0) {
+	else
+	{
+		for (i = 0; i < NR_CMD; i++)
+		{
+			if (strcmp(arg, cmd_table[i].name) == 0)
+			{
 				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
 				return 0;
 			}
@@ -76,20 +160,26 @@ static int cmd_help(char *args) {
 	return 0;
 }
 
-void ui_mainloop() {
-	while(1) {
+void ui_mainloop()
+{
+	while (1)
+	{
 		char *str = rl_gets();
 		char *str_end = str + strlen(str);
 
 		/* extract the first token as the command */
 		char *cmd = strtok(str, " ");
-		if(cmd == NULL) { continue; }
+		if (cmd == NULL)
+		{
+			continue;
+		}
 
 		/* treat the remaining string as the arguments,
 		 * which may need further parsing
 		 */
 		char *args = cmd + strlen(cmd) + 1;
-		if(args >= str_end) {
+		if (args >= str_end)
+		{
 			args = NULL;
 		}
 
@@ -99,13 +189,21 @@ void ui_mainloop() {
 #endif
 
 		int i;
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(cmd, cmd_table[i].name) == 0) {
-				if(cmd_table[i].handler(args) < 0) { return; }
+		for (i = 0; i < NR_CMD; i++)
+		{
+			if (strcmp(cmd, cmd_table[i].name) == 0)
+			{
+				if (cmd_table[i].handler(args) < 0)
+				{
+					return;
+				}
 				break;
 			}
 		}
 
-		if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+		if (i == NR_CMD)
+		{
+			printf("Unknown command '%s'\n", cmd);
+		}
 	}
 }
